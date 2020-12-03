9c81090b9ae062faeb7a841944e98160332d0906
nikos <baroutisnk@gmail.com>
2020-12-03 11:23:09 +0200

    jenkins CI pipeline

--- file1.py
+++ file1.py
@@ -1,4 +1,9 @@
-
+import sys
+import os
+# jenkins exposes the workspace directory through env. with this code can do import
+sys.path.append(os.environ['WORKSPACE'])
+print("------------------------------------------")
+print("python path = ", sys.path.append(os.environ['WORKSPACE']))
 
 def func(a,b):
     if a == b:
