f07eb119ef613b423fa578620b299a0d7df55ab9
nikos <baroutisnk@gmail.com>
2020-12-06 00:23:06 +0200

    jenkins CI pipeline

--- file1.py
+++ file1.py
@@ -1,10 +1,13 @@
-import sys
-import os
 # jenkins exposes the workspace directory through env. with this code can do import
-sys.path.append(os.environ['WORKSPACE'])
+# sys.path.append(os.environ['WORKSPACE'])
 
-def func(a,b):
+def func(a, b):
     if a == b:
         print("are equal")
         return True
+    if a == 0:
+        print("a = 0")
+        return True
+    print("a non zero and  dif than b")
     return False
+
--- tests/test_file1.py
+++ tests/test_file1.py
@@ -2,23 +2,35 @@ import unittest
 import sys
 import os
 # jenkins exposes the workspace directory through env. with this code can do import
-sys.path.append(os.environ['WORKSPACE'])
-import file1 as f
-import coverage
+# sys.path.append(os.environ['WORKSPACE'])
+sys.path.append("/home/nikoscf/PycharmProjects/")
+from file1 import func
 
-coverage.process_startup()
+
 class MyTestCase(unittest.TestCase):
 
     def test_file1_v1(self):
         in1 = 1
         in2 = 1
-        actual = f.func(in1, in2)
+        actual = func(in1, in2)
         self.assertIs(actual, True, "Are equal")
         in1 = 1
         in2 = 2
-        actual = f.func(in1, in2)
+        actual = func(in1, in2)
         self.assertIs(actual, False, "Are not equal")
+        in1 = 0
+        in2 = 2
+        actual = func(in1, in2)
+        self.assertEqual(actual, True, "a = 0")
+
+        in1 = 'a'
+        in2 = 'ab'
+        actual = func(in1, in2)
+        self.assertEqual(actual, False, "No same strings")
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
